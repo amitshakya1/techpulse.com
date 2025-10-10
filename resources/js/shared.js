025f8a3e43f9467b8d2770b42e670ccb50ffd9f1
Amit Shakya <amitshakyaweb@gmail.com>
2025-10-10 19:50:11 +0530

    Added

--- resources/js/shared.js
+++ resources/js/shared.js
@@ -1,5 +1,9 @@
 import './bootstrap';
 
+// Import Bootstrap JavaScript
+import * as bootstrap from 'bootstrap';
+window.bootstrap = bootstrap;
+
 import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
