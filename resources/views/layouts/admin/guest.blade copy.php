d5b00d1e1cd929bf125caf92512312b60b505736
Amit Shakya <amitshakyaweb@gmail.com>
2025-10-11 12:23:18 +0530

    Added

--- resources/views/layouts/admin/guest.blade copy.php
+++ resources/views/layouts/admin/guest.blade copy.php
@@ -18,10 +18,18 @@
     @endif
 </head>
 
-<body x-data="{ page: 'comingSoon', 'loaded': true, 'darkMode': false, 'stickyMenu': false, 'sidebarToggle': false, 'scrollTop': false }" x-init="darkMode = JSON.parse(localStorage.getItem('darkMode'));
-$watch('darkMode', value => localStorage.setItem('darkMode', JSON.stringify(value)))" :class="{ 'dark bg-gray-900': darkMode === true }">
-    <x.admin-preloader />
-    {{ $slot }}
+<body class="font-sans text-gray-900 antialiased">
+    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+        <div>
+            <a href="/">
+                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+            </a>
+        </div>
+
+        <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+            {{ $slot }}
+        </div>
+    </div>
 </body>
 
 </html>
