85165c0171cac53adb68d2420de0e2c31252c3b6
Amit Shakya <amitshakyaweb@gmail.com>
2025-10-18 17:04:44 +0530

    Added

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -2,38 +2,36 @@
     @php
         $formName = 'login-form';
     @endphp
+    <div class="mb-5 sm:mb-8">
+        <x-admin.heading.h1 title="Sign In" />
+        <x-admin.p text="Enter your email and password to sign in!" />
+    </div>
     <div>
-        <div class="mb-5 sm:mb-8">
-            <x-admin.heading.h1 title="Sign In" />
-            <x-admin.p text="Enter your email and password to sign in!" />
-        </div>
-        <div>
-            <x-admin.social-login />
+        <x-admin.social-login />
 
-            <form id="{{ $formName }}" class="space-y-5">
-                <div>
-                    <x-admin.form.input label="Email" type="email" name="email" placeholder="info@gmail.com" />
-                </div>
-                <div>
-                    <x-admin.form.input label="Password" type="password" name="password"
-                        placeholder="Please enter password" />
-                </div>
-                <div class="flex items-center justify-between">
-                    <x-admin.form.checkbox for="remember" name="remember" label="Keep me logged in" />
-                    <x-admin.link href="{{ route('admin.forgot-password') }}" text="Forgot password?" />
-
-                </div>
-                <div>
-                    <x-admin.form.button name="Sign In" />
-                </div>
-            </form>
-            <div id="{{ $formName }}-message"
-                class="hidden relative mt-4 bg-red-50 border border-red-400 text-red-700 px-4 py-3 rounded"></div>
-            <div id="success-message" class="hidden mt-4"></div>
-            <div class="mt-5">
-                <x-admin.p>Don't have an account?
-                    <x-admin.link href="{{ route('admin.register') }}" text="Sign Up" /> </x-admin.p>
+        <form id="{{ $formName }}" class="space-y-5">
+            <div>
+                <x-admin.form.input label="Email" type="email" name="email" placeholder="info@gmail.com" />
             </div>
+            <div>
+                <x-admin.form.input label="Password" type="password" name="password"
+                    placeholder="Please enter password" />
+            </div>
+            <div class="flex items-center justify-between">
+                <x-admin.form.checkbox for="remember" name="remember" label="Keep me logged in" />
+                <x-admin.link href="{{ route('admin.forgot-password') }}" text="Forgot password?" />
+
+            </div>
+            <div>
+                <x-admin.form.button name="Sign In" />
+            </div>
+        </form>
+        <div id="{{ $formName }}-message"
+            class="hidden relative mt-4 bg-red-50 border border-red-400 text-red-700 px-4 py-3 rounded"></div>
+        <div id="success-message" class="hidden mt-4"></div>
+        <div class="mt-5">
+            <x-admin.p>Don't have an account?
+                <x-admin.link href="{{ route('admin.register') }}" text="Sign Up" /> </x-admin.p>
         </div>
     </div>
     @push('script')
